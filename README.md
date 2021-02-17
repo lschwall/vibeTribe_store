f31bafad84ec88b1b6425c23f6a4e64bff4eb387
larryschwall <opsparklschwall@gmail.com>
2021-02-17 15:41:30 -0600

    (add) base files with some stylin

--- README.md
+++
@@ -1,3 +0,0 @@
-# REACT BOILERPLATE
-
-> This includes webpack, babel, React, Socket.io, React-Router, etc
--- server/db/index.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE temp;
-
-CREATE TABLE "customer" (
-  "id" SERIAL PRIMARY KEY,
-    "fill_me_in" varchar,
-);
