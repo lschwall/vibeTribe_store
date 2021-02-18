843d621f09a98423e88c3cfdf711c728029c0178
Larry <opsparklschwall@gmail.com>
2021-02-17 20:27:48 -0600

    (add) burger menu and also sections for merch

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const path = require('path'); 
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`App listening on :${port}`)
+})
